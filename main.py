55f47427776eff679c9faf6d65ab1c6a5cd071f0
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2020-03-09 00:33:38 +0200

    Do scuffed JS formatting

--- main.py
+++ main.py
@@ -1,18 +1,55 @@
+from __future__ import print_function
 import sys
 import pyflate
 
+# Short CSS class names...
+CATEGORY_LITERAL = 'l'
+CATEGORY_REF = 'r'
+
 if len(sys.argv) != 2:
     program = sys.argv[0]
-    print program +':', 'usage:', program, '<filename.gz>'
+    print(program +':', 'usage:', program, '<filename.gz>')
     sys.exit(0)
 
 literal_lengths_map, symbols = pyflate.doit(sys.argv[1])
 
-for c, num_bits in sorted(literal_lengths_map.iteritems(), key=lambda x: x[1]):
-    print('cost of %r = %d bits' % (c, num_bits))
+#for c, num_bits in sorted(literal_lengths_map.iteritems(), key=lambda x: x[1]):
+#    print('cost of %r = %d bits' % (c, num_bits))
 
+indent = 0
+at_newline = False
+bracket_stack = []
 for (symbol, num_bits) in symbols:
     if len(symbol) == 1:
-        print('literal %r - %d bits' % (symbol, num_bits))
+        category = CATEGORY_LITERAL
     else:
-        print('copy %r - %d bits' % (symbol, num_bits))
+        category = CATEGORY_REF
+
+    for c in symbol:
+        if at_newline:
+            print('    ' * indent, end='')
+            at_newline = False
+
+        if c == '{':
+            indent += 1
+            at_newline = True
+            bracket_stack.append('{')
+        elif c == '}':
+            indent -= 1
+            print()
+            print('    ' * indent, end='')
+            at_newline = True
+            bracket_stack.pop()
+        elif c == '(':
+            bracket_stack.append('(')
+        elif c == ')':
+            bracket_stack.pop()
+        elif c == ';':
+            at_newline = True
+        elif c == ',':
+            if len(bracket_stack) == 0 or bracket_stack[-1] == '{':
+                at_newline = True
+
+        print(c, end='')
+        if at_newline:
+            print()
