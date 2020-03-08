10968f8eff796dbb9125714661a0c3d90ac2a6e2
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2020-03-09 00:58:25 +0200

    Colors

--- main.py
+++ main.py
@@ -16,18 +16,38 @@ literal_lengths_map, symbols = pyflate.doit(sys.argv[1])
 #for c, num_bits in sorted(literal_lengths_map.iteritems(), key=lambda x: x[1]):
 #    print('cost of %r = %d bits' % (c, num_bits))
 
+def ansi_color(is_fg, r, g, b):
+    bg_fg = 38 if is_fg else 48
+    return "\x1b[%d;2;%d;%d;%dm" % (bg_fg, r, g, b)
+
+def ansi_reset():
+    print('\x1b[0m', end='')
+
+def color_on(color_tuple):
+    print(ansi_color(True, 0, 0, 0), end='') # Black foreground
+    print(ansi_color(False, *color_tuple), end='') # Background color
+
+def color_off():
+    #print(ansi_color(False, 0, 0, 0), end='') # Black background
+    ansi_reset()
+
 indent = 0
 at_newline = False
 bracket_stack = []
 for (symbol, num_bits) in symbols:
     if len(symbol) == 1:
         category = CATEGORY_LITERAL
+        color = (255, 0, 0)
     else:
         category = CATEGORY_REF
+        color = (0, 0, 255)
 
     for c in symbol:
+        color_on(color)
         if at_newline:
+            color_off()
             print('    ' * indent, end='')
+            color_on(color)
             at_newline = False
 
         if c == '{':
@@ -36,8 +56,11 @@ for (symbol, num_bits) in symbols:
             bracket_stack.append('{')
         elif c == '}':
             indent -= 1
+            color_off()
             print()
+            color_off()
             print('    ' * indent, end='')
+            color_on(color)
             at_newline = True
             bracket_stack.pop()
         elif c == '(':
@@ -52,4 +75,6 @@ for (symbol, num_bits) in symbols:
 
         print(c, end='')
         if at_newline:
+            color_off()
             print()
+ansi_reset()
